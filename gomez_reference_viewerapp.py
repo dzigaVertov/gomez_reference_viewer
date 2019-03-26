1108bc0f093187e01085cb8e6ed5822f68b50d08
marcelo <gomezmarcelod@gmail.com>
2019-03-26 20:03:46 -0300

    agregado filechooser

--- gomez_reference_viewerapp.py
+++ gomez_reference_viewerapp.py
@@ -11,37 +11,50 @@ from kivy.uix.popup import Popup
 from kivy.uix.scatter import Scatter
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stencilview import StencilView
+from kivy.properties import ObjectProperty
 from kivy.config import Config
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
 
-# from kivy.core.window import Window
-
-
 path = '/home/marcelo/Desktop/referencias/fievel/'
+
 
 
 class Sprite(Image):
     def __init__(self, **kwargs):
 
         super(Sprite, self).__init__(**kwargs)
+
+        # para el crop 
         # self.keep_data = True
         # print(type(self.texture))
         # bottomleft = self.texture.get_region(0, 0, 64, 64)
         # self.texture = bottomleft
         self.size = list(map(lambda x: x/2, self.texture_size))
 
+class Archs(Popup):
+    def __init__(self, viejo):
+        super(Archs, self).__init__()
+        self.viejo = viejo
+
+    def eve(self, args):
+        
+        print(args[0].selection)
+        print(self.viejo.ids.vwr)
+        self.viejo.ids.vwr.add_image_wid(args[0].selection[0])
+
 
 class Root_widget(BoxLayout):
-    # def __init__(self):
-    #     self.viewer = Viewer()
 
-    def boton(self):
+    vwr = ObjectProperty(None)
+
+    def agregar(self):
+        self.popi = Archs(self)
+        self.popi.open()
         print('probando')
 
 
 class Referencia(Scatter):
-
     def __init__(self, source):
         super(Referencia, self).__init__()
         self.image = Sprite(source=source, allow_stretch=True)
@@ -54,7 +67,7 @@ class Referencia(Scatter):
         if self.collide_point(x, y) and len(touch.grab_list) == 0:
             super(Referencia, self).on_touch_down(touch)
 
-            if touch.is_mouse_scrolling:  # and touch.grab_current is self:
+            if touch.is_mouse_scrolling: 
                 if touch.button == 'scrolldown':
                     self.scale = 1.1 * self.scale
                 elif touch.button == 'scrollup':
@@ -66,10 +79,6 @@ class Referencia(Scatter):
             self._touches = []
             touch.ungrab(self)
 
-    # def scalar_image(self, factor):
-    #     self.image.size = factor * Vector(*self.image.size)
-    #     self.size = self.image.size
-
 
 class Viewer(StencilView):
     def __init__(self, **kwargs):
@@ -80,13 +89,7 @@ class Viewer(StencilView):
         with self.canvas.after:
             self.refs = [self.add_widget(im) for im in self.images]
 
-        # self.files = FileChooserIconView()
-        # self._popup = Popup(
-        #     title='hola', content=self.files, size_hint=(0.9, 0.9))
-        # self._popup.open()
-
-#        self.add_widget(self.files)
- #       self.files.size = self.size
+    
 
     def load_images(self):
         os.chdir(path)
@@ -94,6 +97,9 @@ class Viewer(StencilView):
             file.endswith('png') or file.endswith('jpg'))]
 
         return images
+
+    def add_image_wid(self, imfile):
+        self.add_widget(Referencia(source=imfile))
 
 
 class gomez_reference_viewerApp(App):
